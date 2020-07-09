3b316698f3903efcb17b01149e8054578ad5fcc5
Mahesh Pawate <mahesh@circle.news>
2020-07-09 18:33:18 +0530

    finished task

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-window.addEventListener('load',()=>{
-    comics()
-})
+asyncFetchComic=async(latest)=>{
+    let response =[]
+    for (let i=0;i<10;i++){
+        try{
+            if(i==5){
+                throw new Error("test")
+            }
+            response[i]=await fetch(`https://xkcd.now.sh/?comic=${latest-i}`)
+            .then((res)=>res.json())
+            console.log(response[i])
+        }
+        catch(err){console.log(err)}
+    }
+}
+
+fetchComic=(latest)=>{
+    let response =[]
+    for (let i=0;i<10;i++){
+        response[i]= fetch(`https://xkcd.now.sh/?comic=${latest-i}`)
+        .then((res)=>res.json())
+    }
+    return Promise.all(response)
+}
 
 comics = () => {
     fetch('https://xkcd.now.sh/?comic=latest')
     .then(res=>res.json())
-    .then(res=>console.log(res))
+    .then(res=>{
+        // fetchComic(res.num)
+        // .then(res=>console.log(res))
+        asyncFetchComic(res.num)
+    })
 }
+
+
+
+window.addEventListener('load',()=>{
+    comics()
+})
